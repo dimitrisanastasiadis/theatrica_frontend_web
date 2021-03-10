b319daba039751a162842e450cfdb46340c3a5aa
dimitris <dimitrisanastasiadis@outlook.com>
2021-03-10 02:40:19 +0200

    Fetching data test from local api

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,14 +1,30 @@
 import { Grid, makeStyles } from "@material-ui/core"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import style from "../assets/jss/layouts/homeStyle"
 import ContentSlider from "../components/ContentSlider"
 import ArtistCard from "../components/ArtistCard"
 import data from "../mockData"
+import axios from "axios"
 
 const useStyles = makeStyles(style)
 
 function Home(props) {
-    const classes = useStyles()
+    const classes = useStyles();
+
+    const [testData, setTestData] = useState([]);
+
+    const getData = async () => {
+        try {
+          const response = await axios.get('http://localhost:8080/api/people');
+          setTestData(response.data.data);
+        } catch (error) {
+          console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
 
     return (
         <Grid container className={classes.grid} justify="center">

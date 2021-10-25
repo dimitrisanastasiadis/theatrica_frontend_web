4b7295125b8d4ea146aaa0a7921e35847fe31bdd
dimitris <dimitrisanastasiadis@outlook.com>
2021-10-25 16:59:35 +0300

    Exported favorite and watchlist logic for shows into custom hook

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -9,54 +9,16 @@ import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
+import useFavoriteShow from "../hooks/useFavoriteShow";
+import useWatchlist from "../hooks/useWatchlist"
 
 const useStyles = makeStyles(style);
 
 function ShowCard({ id, title, media }){
     const classes = useStyles();
 
-    const [isFavorite, setIsFavorite] = useState(false);
-    const [inWatchlist, setInWatchlist] = useState(false);
-
-    useEffect(() => {
-        if (localStorage.getItem("favoriteShows") === null){
-            localStorage.setItem("favoriteShows", JSON.stringify([]));
-        }else{
-            const favoriteShows = JSON.parse(localStorage.favoriteShows);
-            if (favoriteShows.includes(id)){
-                setIsFavorite(true);
-            }
-        }
-
-        if (localStorage.getItem("watchlist") === null){
-            localStorage.setItem("watchlist", JSON.stringify([]));
-        }else{
-            const watchlist = JSON.parse(localStorage.watchlist);
-            if (watchlist.includes(id)){
-                setInWatchlist(true);
-            }
-        }
-    }, [id])
-
-    useEffect(() => {
-        let favoriteShows = JSON.parse(localStorage.favoriteShows);
-        if (isFavorite){
-            favoriteShows.push(id);
-        }else{
-            favoriteShows = favoriteShows.filter(item => item !== id);
-        }
-        localStorage.favoriteShows = JSON.stringify(favoriteShows);
-    }, [isFavorite, id])
-
-    useEffect(() => {
-        let watchlist = JSON.parse(localStorage.watchlist);
-        if (inWatchlist){
-            watchlist.push(id);
-        }else{
-            watchlist = watchlist.filter(item => item !== id);
-        }
-        localStorage.watchlist = JSON.stringify(watchlist);
-    }, [inWatchlist, id])
+    const { isFavorite, setIsFavorite } = useFavoriteShow(id);
+    const { inWatchlist, setInWatchlist } = useWatchlist(id);
 
     const handleFavorite = () => {
         setIsFavorite(prev => !prev);

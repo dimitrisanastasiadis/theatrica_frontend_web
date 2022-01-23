bcee85c557c11ebe532cc76084a22d52bbc63acb
dimitris <dimitrisanastasiadis@outlook.com>
2022-01-23 12:14:54 +0200

    Updated 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,17 @@
+import NextLink from "next/link"
+import { Link } from "@material-ui/core"
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <div className="pageWrapper">
+      <div className="pageContent">
+       <h1>404 - Δεν βρέθηκε η σελίδα!</h1>
+        <NextLink href="/" passHref>
+          <Link variant="body1" color="secondary">
+            Επιστροφή στην Αρχική
+          </Link>
+        </NextLink>
+      </div>
+    </div>
+  )
 }

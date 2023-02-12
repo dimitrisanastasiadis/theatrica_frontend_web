14e965650a2a97956f71df5bd51f05bf7d9d3bdb
dimitrisanastasiadis <dimitrisanastasiadis@outlook.com>
2023-02-12 16:57:02 +0200

    Change tab spacing

--- pages/index.js
+++ pages/index.js
@@ -14,134 +14,133 @@ import SearchIcon from "@material-ui/icons/Search"
 import { useRouter } from "next/router"
 
 export const getStaticProps = async () => {
-    cloudinary.config({
-        cloud_name: 'dpddl5vyr',
-        secure: true
+  cloudinary.config({
+    cloud_name: 'dpddl5vyr',
+    secure: true
+  })
+
+  const articlesResponse = await newsFetcher(encodeURI("/everything?q=παράσταση θέατρο&sortBy=publishedAt&pageSize=3"))
+  const articles = articlesResponse?.articles
+
+  articles.forEach(article => {
+    article.urlToImage = cloudinary.url(
+      encodeURI(article.urlToImage),
+      {
+        type: "fetch", width: 320, fetch_format: "auto", crop: "scale", quality: "auto"
+      })
+  })
+
+
+  const artistIDs = [5555, 6846, 4770, 4791, 8158, 5047, 5233, 5428, 4691, 5192, 4962, 6643, 4659, 6104];
+  let artists = await Promise.all(
+    artistIDs.map(async id => {
+      const artist = await mainFetcher(`/people/${id}`);
+      return artist;
     })
+  )
 
-    const articlesResponse = await newsFetcher(encodeURI("/everything?q=παράσταση θέατρο&sortBy=publishedAt&pageSize=3"))
-    const articles = articlesResponse?.articles
+  artists = artists.filter(Boolean);
 
-    articles.forEach(article => {
-        article.urlToImage = cloudinary.url(
-            encodeURI(article.urlToImage),
-            {
-                type: "fetch", width: 320, fetch_format: "auto", crop: "scale", quality: "auto"
-            })
-    })
+  let latestShows = await mainFetcher(`/productions?page=0&size=10`)
 
+  latestShows = latestShows?.content?.map(show => ({
+    id: show.id,
+    title: show.title,
+    image: getShowImage(show.mediaURL)
+  }))
 
-    const artistIDs = [5555, 6846, 4770, 4791, 8158, 5047, 5233, 5428, 4691, 5192, 4962, 6643, 4659, 6104];
-    let artists = await Promise.all(
-        artistIDs.map(async id => {
-            const artist = await mainFetcher(`/people/${id}`);
-            return artist;
-        })
-    )
-
-    artists = artists.filter(Boolean);
-
-    let latestShows = await mainFetcher(`/productions?page=0&size=10`)
-
-    latestShows = latestShows?.content?.map(show => ({
-            id: show.id,
-            title: show.title,
-            image: getShowImage(show.mediaURL)
-        }))
-
-    return {
-        props: { artists, latestShows, articles },
-        revalidate: 60 * 15
-    }
+  return {
+    props: { artists, latestShows, articles },
+    revalidate: 60 * 15
+  }
 }
 
 const useStyles = makeStyles(style)
 
 function Home({ artists, latestShows, articles }) {
-    const classes = useStyles();
-    const [searchValue, setSearchValue] = useState("")
-    const router = useRouter()
+  const classes = useStyles();
+  const [searchValue, setSearchValue] = useState("")
+  const router = useRouter()
 
-    const handleChange = event => {
-        setSearchValue(event.target.value)
-    }
+  const handleChange = event => {
+    setSearchValue(event.target.value)
+  }
 
-    const handleSubmit = event => {
-        event.preventDefault()
-        if (searchValue)
-            router.push(`/results?search_query=${searchValue}`)
-    }
+  const handleSubmit = event => {
+    event.preventDefault()
+    if (searchValue)
+      router.push(`/results?search_query=${searchValue}`)
+  }
 
-    return (
-        <>
-            <Head>
-                <title>Theatrica</title>
-                <meta name="description" content="Αναζητήστε καλλιτέχνες, παραστάσεις και θέατρα, δείτε στατιστικά και συγκρίνετε χρονικές περιόδους ή βρείτε μια παράσταση στην περιοχή σας!" />
-            </Head>
-            <div className={classes.wrapper}>
-                <div className={classes.container}>
-                    <div className={classes.heroSection}>
-                        <Typography variant="h1">Theatrica</Typography>
-                        <Typography variant="body2">Αναζητήστε καλλιτέχνες, παραστάσεις και θέατρα, δείτε στατιστικά και συγκρίνετε χρονικές περιόδους ή βρείτε μια παράσταση στην περιοχή σας!</Typography>
-                        <form onSubmit={handleSubmit}>
-                            <div className={classes.searchInput}>
-                                <SearchIcon />
-                                <InputBase
-                                    type="text"
-                                    placeholder="Αναζήτηση..."
-                                    value={searchValue}
-                                    onChange={handleChange} />
-                            </div>
+  return (
+    <>
+      <Head>
+        <title>Theatrica</title>
+        <meta name="description" content="Αναζητήστε καλλιτέχνες, παραστάσεις και θέατρα, δείτε στατιστικά και συγκρίνετε χρονικές περιόδους ή βρείτε μια παράσταση στην περιοχή σας!" />
+      </Head>
+      <div className={classes.wrapper}>
+        <div className={classes.container}>
+          <div className={classes.heroSection}>
+            <Typography variant="h1">Theatrica</Typography>
+            <Typography variant="body2">Αναζητήστε καλλιτέχνες, παραστάσεις και θέατρα, δείτε στατιστικά και συγκρίνετε χρονικές περιόδους ή βρείτε μια παράσταση στην περιοχή σας!</Typography>
+            <form onSubmit={handleSubmit}>
+              <div className={classes.searchInput}>
+                <SearchIcon />
+                <InputBase
+                  type="text"
+                  placeholder="Αναζήτηση..."
+                  value={searchValue}
+                  onChange={handleChange} />
+              </div>
 
-                        </form>
-                    </div>
-                    <section>
-                        <Typography className={classes.headingPadding} variant="h3" component="h2">Νεότερες Ειδήσεις</Typography>
-                        <div className={classes.newsContainer}>
-                            {articles.map(article =>
-                                <NewsCard
-                                    key={article.url}
-                                    article={article}
-                                />
-                            )
-                            }
-                        </div>
-                    </section>
-                    <Hidden smDown>
-                        <Divider className={classes.divider} flexItem />
-                    </Hidden>
-                    <section>
-                        <ContentSlider title="Καλλιτέχνες" description="Δημοφιλείς Ηθοποιοί">
-                            {artists.map((artist, index) =>
-                                <ArtistCard
-                                    id={artist.id}
-                                    fullName={artist.fullName}
-                                    image={artist.image}
-                                    key={index}
-                                    delay={index}
-                                />
-                            )}
-                        </ContentSlider>
-                    </section>
-                    <Hidden smDown>
-                        <Divider className={classes.divider} flexItem />
-                    </Hidden>
-                    <section>
-                        <ContentSlider title="Παραστάσεις" description="Νέες Κυκλοφορίες">
-                            {latestShows.map((item) =>
-                                <ShowCard
-                                    id={item.id}
-                                    title={item.title}
-                                    media={item.image}
-                                    key={item.id}
-                                />)}
-                        </ContentSlider>
-                    </section>
-                </div>
+            </form>
+          </div>
+          <section>
+            <Typography className={classes.headingPadding} variant="h3" component="h2">Νεότερες Ειδήσεις</Typography>
+            <div className={classes.newsContainer}>
+              {articles.map(article =>
+                <NewsCard
+                  key={article.url}
+                  article={article}
+                />
+              )
+              }
             </div>
-        </>
-
-    )
+          </section>
+          <Hidden smDown>
+            <Divider className={classes.divider} flexItem />
+          </Hidden>
+          <section>
+            <ContentSlider title="Καλλιτέχνες" description="Δημοφιλείς Ηθοποιοί">
+              {artists.map((artist, index) =>
+                <ArtistCard
+                  id={artist.id}
+                  fullName={artist.fullName}
+                  image={artist.image}
+                  key={index}
+                  delay={index}
+                />
+              )}
+            </ContentSlider>
+          </section>
+          <Hidden smDown>
+            <Divider className={classes.divider} flexItem />
+          </Hidden>
+          <section>
+            <ContentSlider title="Παραστάσεις" description="Νέες Κυκλοφορίες">
+              {latestShows.map((item) =>
+                <ShowCard
+                  id={item.id}
+                  title={item.title}
+                  media={item.image}
+                  key={item.id}
+                />)}
+            </ContentSlider>
+          </section>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home;
